3cfcf0911a6d38ddc5e86e95993f46cba718afe0
rogersjr <rogers.johnsonjr@verizon.net>
2015-01-25 19:53:17 -0500

    add run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,5 @@
+X_test <- read.table("./quiz31/Project3/UCI HAR Dataset/test/X_test.txt")
+#Have not figured out how to read y_train.txt
+features <- read.table("./quiz31/Project3/UCI HAR Dataset/features.txt")
+X_train <- read.table("./quiz31/Project3/UCI HAR Dataset/train/X_train.txt")
+#Have not figured out how to read y_test.txt
